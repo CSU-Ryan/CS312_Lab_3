89eeb7c4bbace8f1a892b54fa159492535d627db
CSU-Ryan <enderbro@colostate.edu>
2026-03-28 20:36:44 -0600

    Clone over lab 2

---
+++ local_html/east/foods.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Favorite Foods</title>
+    <meta name="author" content="Ryan Grimm">
+    <meta name="description" content="Ryan Grimm's favorite foods">
+    <meta name="keywords" content="ryan grimm, CSU, computer science, favorite food">
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+
+<header>
+    <h1>Ryan's Favorite Foods</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="foods.php">Foods</a></li>
+        <li><a href="../west/foods.php">Switch</a></li>
+    </ul>
+</nav>
+
+<main>
+    <h2>Dinners</h2>
+    <ul>
+        <li>Tomato Rice</li>
+        <li>Lasagna</li>
+        <li>Lemon-Herb Baked Chicken</li>
+    </ul>
+
+    <h2>Breads</h2>
+    <p>
+        <a href=https://www.kingarthurbaking.com/recipes/no-knead-crusty-white-bread-recipe>
+            Crusty No-Knead (King Arthur)
+        </a>
+    </p>
+    <blockquote cite="https://www.kingarthurbaking.com/recipes/no-knead-crusty-white-bread-recipe">
+        <p>
+            "The most basic of all no-knead crusty bread recipes, this is a wonderful way to get into yeast-bread baking. The easy stir-together dough rests in your refrigerator, developing flavor all the time, till you're ready to bake. About 90 minutes before you want to serve bread, grab a handful of dough, shape it, let it rise, then bake for 30 minutes. The result? Incredible, artisan-style crusty bread, worthy of its <strong>2016 Recipe of the Year</strong> title."
+        </p>
+        <p>&mdash;<cite>King Arthur Flour</cite></p>
+    </blockquote>
+
+    <img src="https://www.kingarthurbaking.com/sites/default/files/styles/featured_image_2x/public/2026-02/No-Knead-Crusty-White-Bread-12.jpg?itok=2U0spxgP"
+        alt="King Arthur No-Knead Crusty White Bread"
+        height=200pt>
+</main>
+
+<footer>
+    <p>
+        <img src="../resources/CSU-Symbol.png" alt="" height=48pt>
+        <a href="mailto://ryan.grimm@colostate.edu">Ryan.Grimm@colostate.edu</a>
+        | CSU Mathematics Undergraduate
+    </p>
+</footer>
+
+</body>
+</html>
