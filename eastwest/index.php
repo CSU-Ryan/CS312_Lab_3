864d71e7de771ddf030331b79bfa5da19be7e172
CSU-Ryan <enderbro@colostate.edu>
2026-03-28 21:58:35 -0600

    modified

--- eastwest/index.php
+++ eastwest/index.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 
-<html lang="en">
 <?php
 // Setup for default page
 $_GET["direction"] = "east";
 $_POST["page"] = "index";
 ?>
 
-<?php include "./content/head.php"; ?>
+<html lang="en">
+<?php // include "./content/head.php"; ?>
 
 <body>
 
-<p> Header below! </p>
-<?php // include "./content/header.php"; ?>
+    <p> Header below! </p>
+    <?php // include "./content/header.php"; ?>
 
 </body>
-
 </html>
