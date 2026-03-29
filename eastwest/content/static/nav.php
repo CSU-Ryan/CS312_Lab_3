6d06b62f449b0b3190bdb22d07bf1258fc0991da
CSU-Ryan <enderbro@colostate.edu>
2026-03-29 16:52:38 -0600

    Temporarily uses $_GET to implement page links

--- eastwest/content/static/nav.php
+++ eastwest/content/static/nav.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Foods</li>
+        <li><a href="index.php?page=home">Home</a></li>
+        <li><a href="index.php?page=about">About</a></li>
+        <li><a href="index.php?page=foods">Foods</a></li>
         <li><a href="index.php?direction=<?php echo $other_direction; ?>">Switch</a></li>
     </ul>
 </nav>
