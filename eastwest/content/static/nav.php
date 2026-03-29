3f7229f4d73f014f3df3c18fcb16523419fec93e
CSU-Ryan <enderbro@colostate.edu>
2026-03-29 17:04:17 -0600

    Corrects linking functionality

--- eastwest/content/static/nav.php
+++ eastwest/content/static/nav.php
@@ -1,18 +1,14 @@
 <?php
-function toPage($toPage) {
-    return "index.php?direction={$direction}&page={$toPage}";
-}
-
-function swap() {
-    return "index.php?direction={$other_direction}&page={$page}";
+function linkTo($direction, $page) {
+    return "index.php?direction={$direction}&page={$page}";
 }
 ?>
 
 <nav>
     <ul>
-        <li><a href="<?php echo toPage("home"); ?>">Home</a></li>
-        <li><a href="<?php echo toPage("about"); ?>">About</a></li>
-        <li><a href="<?php echo toPage("foods"); ?>">Foods</a></li>
-        <li><a href="<?php echo swap(); ?>">Switch</a></li>
+        <li><a href="<?php echo linkTo($direction, "home"); ?>">Home</a></li>
+        <li><a href="<?php echo linkTo($direction, "about"); ?>">About</a></li>
+        <li><a href="<?php echo linkTo($direction, "foods"); ?>">Foods</a></li>
+        <li><a href="<?php echo linkTo($other_direction, $page); ?>">Switch</a></li>
     </ul>
 </nav>
