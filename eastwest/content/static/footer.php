121c45c7d6b5c116d9802954ee1335fa5487fe67
CSU-Ryan <enderbro@colostate.edu>
2026-03-29 16:48:31 -0600

    Corrects image paths

--- eastwest/content/static/footer.php
+++ eastwest/content/static/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p>
-        <img src="../resources/CSU-Symbol.png" alt="" height=48pt>
+        <img src="./resources/CSU-Symbol.png" alt="" height=48pt>
         <a href="mailto://ryan.grimm@colostate.edu">Ryan.Grimm@colostate.edu</a>
         | CSU Mathematics Undergraduate
     </p>
