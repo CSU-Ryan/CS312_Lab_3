eea45eb29da34310396906e6432915dcb00d3267
CSU-Ryan <enderbro@colostate.edu>
2026-03-28 21:34:41 -0600

    Corrects php delimiters

--- eastwest/content/head.php
+++ eastwest/content/head.php
@@ -6,5 +6,5 @@
     <meta name="author" content="Ryan Grimm">
     <meta name="description" content="Ryan Grimm homepage">
     <meta name="keywords" content="ryan grimm, CSU, computer science">
-    <link rel="stylesheet" type="text/css" href="./<?php echo $_GET[direction]; >.css">
+    <link rel="stylesheet" type="text/css" href="./<?php echo $_GET[direction]; ?>.css">
 </head>
