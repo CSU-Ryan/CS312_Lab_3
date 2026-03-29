db8fb4720681904c66b5dd094ae0f774233c5fcc
CSU-Ryan <enderbro@colostate.edu>
2026-03-28 21:48:48 -0600

    Reformats index.php

--- eastwest/index.php
+++ eastwest/index.php
@@ -3,8 +3,12 @@
 // Setup for default page
 $_GET["direction"] = "east";
 $_POST["page"] = "index";
-
-include "./content/head.php";
-
-include "./content/header.php";
 ?>
+
+<?php include "./content/head.php"; ?>
+
+<body>
+
+<?php include "./content/header.php"; ?>
+
+</body>
