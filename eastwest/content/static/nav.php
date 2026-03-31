30799b685d51b9403b9967898f13b3e3ded9d982
CSU-Ryan <enderbro@colostate.edu>
2026-03-31 11:45:48 -0600

    Corrects buttons to preserve direction

--- eastwest/content/static/nav.php
+++ eastwest/content/static/nav.php
@@ -1,29 +1,29 @@
 <?php
-function linkTo($direction, $page) {
-    return "?direction={$direction}&page={$page}";
+function linkTo($direction) {
+    return "?direction={$direction}";
 }
 ?>
 
 <nav>
     <ul>
         <li>
-            <form method="post" action="index.php">
+            <form method="post" action="?direction=<?php echo $direction; ?>">
                 <input type="hidden" name="page" value="home">
                 <input type="submit" value="Home">
             </form>
         </li>
         <li>
-            <form method="post" action="index.php">
+            <form method="post" action="?direction=<?php echo $direction; ?>">
                 <input type="hidden" name="page" value="about">
                 <input type="submit" value="About">
             </form>
         </li>
         <li>
-            <form method="post" action="index.php">
+            <form method="post" action="?direction=<?php echo $direction; ?>">
                 <input type="hidden" name="page" value="foods">
                 <input type="submit" value="Foods">
             </form>
         </li>
-        <li><a href="<?php echo linkTo($other_direction, $page); ?>">Switch</a></li>
+        <li><a href="direction=<?php echo $direction; ?>">Switch</a></li>
     </ul>
 </nav>
